7d4ba69baa9a7374b626df9d3ddea98bddb15e3e
kellerkatlin <kellerkatlin.k@gmail.com>
2025-08-24 21:39:45 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import Index from "./page/index";
+import Index from "./page/Index";
 import NotFound from "./page/NotFound";
 function App() {
   const queryClient = new QueryClient();
